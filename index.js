cc8857bc1c5fdef6fa8f8adadd183ad0d23b8509
marls628 <marlyshawley@outlook.com>
2017-11-14 02:23:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Write your solution here!
  const drivers = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendDriver(name) {
-  drivers.push("Ralph");
-}
 function destructivelyPrependDriver(name) {
   drivers.unshitft("Bob");
 }
